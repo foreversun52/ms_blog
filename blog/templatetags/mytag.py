4a73187114a21063a9a406f0f56914af0e1544b3
forever <1479487347@qq.com>
2020-03-19 10:36:29 +0800

    first

---
+++ blog/templatetags/mytag.py
@@ -0,0 +1,20 @@
+from django.template import Library
+from blog import models
+from django.db.models.functions import TruncMonth
+from django.db.models import Count
+
+register = Library()
+
+
+@register.inclusion_tag('left_menu.html')
+def index(username):
+    user_obj = models.UserInfo.objects.filter(username=username).first()
+    blog = user_obj.blog
+    # 1.查询当前用户所有的分类及分类下的文章数
+    category_list = models.Category.objects.filter(blog=blog).annotate(count_num=Count('article__pk')).values_list(
+        'name', 'count_num', 'pk')
+
+    # 2.按照年月统计文章数
+    date_list = models.Article.objects.filter(blog=blog).annotate(month=TruncMonth('create_time')).values(
+        'month').annotate(count_num=Count('pk')).order_by('-month').values_list('month', 'count_num')
+    return locals()
